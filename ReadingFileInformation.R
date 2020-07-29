7184a1c4ba9bc9a8f7dfc71f5ab85e589524e8d2
gilhuesca <gilhuesca-github@yahoo.fr>
2020-07-29 11:26:20 -0500

    Converting ? to NA

--- ReadingFileInformation.R
+++ ReadingFileInformation.R
@@ -1,6 +1,6 @@
 #reading file. Only the rows for dates 2007-02-01 and 2007-02-02
 
-data<-read.table("household_power_consumption.txt",skip=66637,nrows=2880,sep = ";",col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+data<-read.table("household_power_consumption.txt",skip=66637,nrows=2880,sep = ";",col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),na.strings = c("?"))
 head(data)
 tail(data)
 
@@ -13,3 +13,5 @@ data$Date<- as.Date(data$Date,format="%d/%m/%Y")
 #It keeps the current date
 #see if that's a problem
 data$Time<- strptime(data$Time,format = "%H:%M:%S")
+
+    
