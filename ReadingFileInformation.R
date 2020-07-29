9fabe2db30a428038322c42225c54cf340ee70c3
gilhuesca <gilhuesca-github@yahoo.fr>
2020-07-29 11:20:10 -0500

    Converting dates and times

--- ReadingFileInformation.R
+++ ReadingFileInformation.R
@@ -4,5 +4,12 @@ data<-read.table("household_power_consumption.txt",skip=66637,nrows=2880,sep = "
 head(data)
 tail(data)
 
+#You may find it useful to convert the Date and Time variables to Date/Time classes in R using the \color{red}{\verb|strptime()|}strptime()  and \color{red}{\verb|as.Date()|}as.Date() functions.
 
+#converting dates
+data$Date<- as.Date(data$Date,format="%d/%m/%Y")
 
+#converting times.
+#It keeps the current date
+#see if that's a problem
+data$Time<- strptime(data$Time,format = "%H:%M:%S")
